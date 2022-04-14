4e70137d3e8580958396b1dccc3296128c90a5ef
Matheus Sbaraglini <matheus.sbaraglini@hotmail.com>
2022-04-14 20:48:39 -0300

    implment basic checkout route without domain logic

--- internal/infrastructure/server/http/checkout.go
+++ internal/infrastructure/server/http/checkout.go
@@ -4,8 +4,16 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/matheussbaraglini/hash-challenge/internal/domain"
 )
 
 func (h *handler) checkout(c *gin.Context) {
-	c.AbortWithStatus(http.StatusOK)
+	products := &domain.ProductInput{}
+	if err := c.ShouldBindJSON(&products); err != nil {
+		c.AbortWithStatus(http.StatusBadRequest)
+	}
+
+	// add products
+
+	c.JSON(http.StatusOK, products) // temp
 }
